27f0bdc93a07bbdbc2877aa4986feb5d33e337e6
yuberee <59583743+yuberee@users.noreply.github.com>
2025-01-12 17:31:22 +0100

    ragdolle

--- Code/Actor.cs
+++ Code/Actor.cs
@@ -1,4 +1,6 @@
 using Sandbox;
+using Sandbox.UI;
+using System.Xml.Linq;
 
 public abstract partial class Actor : Component
 {
@@ -31,8 +33,54 @@ public abstract partial class Actor : Component
 		LastName = randomName.Item2;
 	}
 
+	[Property]
+	[Category( "Components" )]
+	public SkinnedModelRenderer ModelRenderer { get; set; }
+
+	[Property]
+	[Category( "Components" )]
+	public BoxCollider Collider { get; set; }
+
+	private ModelPhysics _ragdoll;
+
 	protected override void OnUpdate()
 	{
 
+	}
+
+	protected override void DrawGizmos()
+	{
+		var draw = Gizmo.Draw;
+
+		var nameTransform = global::Transform.Zero
+			.WithPosition( Vector3.Up * 84f )
+			.WithRotation( Rotation.FromYaw( 90f ) * Rotation.FromRoll( 90f ) );
+
+		draw.WorldText( FullName, nameTransform, "Poppins", 12f );
+	}
+
+	[Category( "Debug" )]
+	[Button( "Ragdoll", "person_off" )]
+	public void Ragdoll()
+	{
+		if ( !ModelRenderer.IsValid() ) return;
+		if ( _ragdoll.IsValid() ) return;
+
+		_ragdoll = AddComponent<ModelPhysics>();
+		_ragdoll.Renderer = ModelRenderer;
+		_ragdoll.Model = ModelRenderer.Model;
+
+		Collider.Enabled = false;
+	}
+
+	[Category( "Debug" )]
+	[Button( "Unragdoll", "person_outline" )]
+	public void Unragdoll()
+	{
+		if ( !ModelRenderer.IsValid() ) return;
+		if ( !_ragdoll.IsValid() ) return;
+
+		_ragdoll.Destroy();
+		Collider.Enabled = true;
 	}
 }
