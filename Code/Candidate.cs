2afac2be8430a1fe4dabdb45d6843ad4dd254d33
yuberee <59583743+yuberee@users.noreply.github.com>
2025-01-14 22:01:13 +0100

    Candidate policies, gender, steamid

--- Code/Candidate.cs
+++ Code/Candidate.cs
@@ -2,14 +2,59 @@
 [Icon( "ballot" )]
 public struct Candidate
 {
+	/// <summary>
+	/// Unique ID for the candidate, used for votes too
+	/// </summary>
 	[Property]
 	public int CandidateId { get; set; }
 
+	/// <summary>
+	/// Name/Username of candidate
+	/// </summary>
 	[Property]
 	public string CandidateName { get; set; }
 
+	/// <summary>
+	/// For pronouns
+	/// </summary>
+	[Property]
+	public Gender CandidateGender { get; set; }
+
+	/// <summary>
+	/// Steam id of the candidate, for in game interactions
+	/// </summary>
+	[Property]
+	public ulong CandidateSteamId { get; set; }
+
+	/// <summary>
+	/// The candidate NPC
+	/// </summary>
 	[Property]
 	public GameObject CandidatePrefab { get; set; }
+
+	public struct Policy
+	{
+		/// <summary>
+		/// The short name of this policy
+		/// </summary>
+		[Property]
+		public string Name { get; set; }
+
+		/// <summary>
+		/// A description on what the policy is or does, no period needed at the end
+		/// </summary>
+		[Property]
+		[WideMode]
+		public string Info { get; set; }
+	}
+
+	/// <summary>
+	/// The policies of this candidate
+	/// </summary>
+	[Property]
+	[InlineEditor]
+	[WideMode]
+	public List<Policy> CandidatePolicies { get; set; }
 
 	public override string ToString() => $"[{CandidateId}] {CandidateName}";
 }
