117f2696aa0413df25310488fb52294083272152
yuberee <59583743+yuberee@users.noreply.github.com>
2025-01-14 22:01:19 +0100

    Random voter pick

--- Code/Voter.cs
+++ Code/Voter.cs
@@ -5,4 +5,20 @@ using System.Xml.Linq;
 
 public partial class Voter : Actor
 {
+	/// <summary>
+	///  Who this voter is going to vote
+	/// </summary>
+	public Candidate Pick { get; private set; }
+
+	protected override void OnStart()
+	{
+		base.OnStart();
+
+		Pick = ElectionsManager.RandomCandidate();
+	}
+
+	public void Talk()
+	{
+		Log.Info( Game.Random.FromList( InteractPhrases ) );
+	}
 }
