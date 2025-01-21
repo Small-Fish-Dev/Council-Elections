a7751fcf448c61966a4a3cc2425ac9a61b4fca10
yuberee <59583743+yuberee@users.noreply.github.com>
2025-01-21 21:38:54 +0100

    npc gizmos

--- Code/Npcmanager.cs
+++ Code/Npcmanager.cs
@@ -34,9 +34,39 @@ public sealed class Npcmanager : Component
 		}
 	}
 
-
-	protected override void OnUpdate()
+	public void SpawnVoter( Vector3 position )
 	{
+		var toSpawn = Game.Random.FromList( Voters );
+		var spawned = toSpawn.Clone( position );
 
+		if ( spawned.Components.TryGet<Actor>( out var actor ) )
+			actor.WalkTo( position );
+	}
+
+	protected override void DrawGizmos()
+	{
+		using ( Gizmo.Scope( "world", global::Transform.Zero ) )
+		{
+			var draw = Gizmo.Draw;
+
+			foreach ( var linePoint in LinePoints )
+			{
+				draw.Color = Color.Yellow.WithAlpha( 0.5f );
+				draw.SolidSphere( linePoint.WorldPosition, 12f, 32, 32 );
+				draw.Color = Color.White;
+				var textPos = linePoint.WorldPosition + Vector3.Up * 5f;
+				var textRot = linePoint.WorldRotation * Rotation.FromYaw( 270f ) * Rotation.FromRoll( 90f );
+				draw.WorldText( LinePoints.IndexOf( linePoint ).ToString(), new Transform( textPos, textRot ) );
+			}
+
+			draw.Color = Color.Green.WithAlpha( 0.5f );
+			draw.SolidSphere( NpcSpawn.WorldPosition, 20f, 32, 32 );
+
+			draw.Color = Color.Blue.WithAlpha( 0.5f );
+			draw.SolidSphere( NpcInterior.WorldPosition, 20f, 32, 32 );
+
+			draw.Color = Color.Red.WithAlpha( 0.5f );
+			draw.SolidSphere( NpcDespawn.WorldPosition, 20f, 32, 32 );
+		}
 	}
 }
